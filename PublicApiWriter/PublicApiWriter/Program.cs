5308cd2975438817d6adec10eacb1a18177cc9f5
GrahamTheCoder <grahamthecoder@gmail.com>
2015-09-10 14:34:16 +0100

    Remove unused

--- PublicApiWriter/PublicApiWriter/Program.cs
+++ PublicApiWriter/PublicApiWriter/Program.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AssemblyApi.Output;
-using Microsoft.CodeAnalysis.MSBuild;
 
 namespace AssemblyApi
 {
