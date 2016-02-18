9e8656dbd8787122c9fdc596d8b62efcd3a64bdf
GrahamTheCoder <grahamthecoder@gmail.com>
2016-02-18 14:55:24 +0000

    Basic test that this test method can be found

---
+++ PublicApiWriter/AssemblyApiTests/MainTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssemblyApi;
+using AssemblyApiTests.Utils;
+using NUnit.Framework;
+
+namespace AssemblyApiTests
+{
+    [TestFixture]
+    public class MainTests
+    {
+        private readonly FileInfo m_SolutionFile;
+
+        public MainTests()
+        {
+            var solutionDirectory = new DirectoryInfo(Environment.CurrentDirectory + @"\..\..\");
+            m_SolutionFile = solutionDirectory.GetFiles("*.csproj").First();
+        }
+
+        [Test]
+        public void AnalyzingSelfFindsThisMethodAndOtherStuff()
+        {
+            using (var tempFileManager = new TempFileManager())
+            {
+                var outputFile = tempFileManager.GetNew();
+                RunMain(m_SolutionFile.FullName, outputFile.FullName, "TestCode;AssemblyApiTests", @"OurCode;AssemblyApi\.");
+                var lines = File.ReadAllLines(outputFile.FullName);
+
+                var linesContainingThisMethod = lines.Where(l => l.Contains(nameof(AnalyzingSelfFindsThisMethodAndOtherStuff))).ToList();
+                Assert.That(linesContainingThisMethod, Has.Count.EqualTo(1));
+
+                Assert.That(lines, Has.Length.GreaterThan(15));
+            }
+        }
+
+        private static void RunMain(string solutionPath, string outputFile, string inclusionRegexes, string exclusionRegexes)
+        {
+            Program.Main(new [] {solutionPath, outputFile, inclusionRegexes, exclusionRegexes});
+        }
+    }
+}
---
+++ PublicApiWriter/AssemblyApiTests/Utils/TempFileManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AssemblyApiTests.Utils
+{
+    public class TempFileManager : IDisposable
+    {
+        private readonly List<FileInfo> m_Files = new List<FileInfo>();
+        private DirectoryInfo m_TempDirectory;
+
+        public TempFileManager()
+        {
+            var randomDirectoryName = Path.GetTempFileName().Substring(0,8);
+            var newTempDirectoryPath = Path.Combine(Path.GetTempPath(), randomDirectoryName);
+            m_TempDirectory = new DirectoryInfo(newTempDirectoryPath);
+            m_TempDirectory.Create();
+        }
+
+
+        public FileInfo GetNew()
+        {
+            var newTempFilePath = Path.Combine(m_TempDirectory.FullName, Path.GetTempFileName());
+            var fileInfo = new FileInfo(newTempFilePath);
+            m_Files.Add(fileInfo);
+            return fileInfo;
+        }
+
+        public void Dispose()
+        {
+            foreach (var fileInfo in m_Files)
+            {
+                fileInfo.Delete();
+            }
+        }
+    }
+}
