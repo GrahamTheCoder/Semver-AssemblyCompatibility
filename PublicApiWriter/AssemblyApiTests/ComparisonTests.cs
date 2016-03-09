28dad2b985fdee5ecdcb55a84ff18815c3a7649b
GrahamTheCoder <grahamthecoder@gmail.com>
2016-03-09 14:35:26 +0000

    Add the first semblance of comparison
    The next goal is to create a semantic version from the differences, and describe which differences caused the version to change.

---
+++ PublicApiWriter/AssemblyApiTests/ComparisonTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssemblyApi.Comparison;
+using AssemblyApi.ModelBuilder;
+using AssemblyApi.Output;
+using AssemblyApiTests.Builders;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using static Microsoft.CodeAnalysis.Accessibility;
+using static Microsoft.CodeAnalysis.SymbolKind;
+
+
+namespace AssemblyApiTests
+{
+    [TestFixture]
+    public class ComparisonTests
+    {
+        [Test]
+        public void ComparisonIsEqual()
+        {
+            var oldApi = CreateApi("1");
+            var newApi = CreateApi("1");
+            var comparison = Compare(oldApi, newApi);
+            Assert.That(comparison, Is.Not.Empty);
+        }
+
+        private static IReadOnlyCollection<ApiNodeComparison> Compare(ApiNode oldApi, ApiNode newApi)
+        {
+            return ApiNodeComparison.Compare(new[] {oldApi}, new[] {newApi});
+        }
+
+        private static ApiNode CreateApi(string uniquebit)
+        {
+            var type = new ApiNodeBuilder(NamedType, Public, $"Type{uniquebit}");
+            var namespaceNode = new ApiNodeBuilder(Namespace, Public, $"Namespace{uniquebit}").WithMembers(type);
+            var assemblyNode = new ApiNodeBuilder(Assembly, Public, $"Assembly{uniquebit}").WithMembers(namespaceNode).Build();
+            return assemblyNode;
+        }
+    }
+}
---
+++ PublicApiWriter/PublicApiWriter/Comparison/ApiNodeComparison.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AssemblyApi.ModelBuilder;
+using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+
+namespace AssemblyApi.Comparison
+{
+    internal sealed class ApiNodeComparison
+    {
+        public ApiNode OldApiNode { get;}
+        public ApiNode NewApiNode { get;}
+        public IReadOnlyCollection<ApiNodeComparison> MemberComparison{ get; }
+
+        private ApiNodeComparison([CanBeNull] ApiNode oldApiNode, [CanBeNull] ApiNode newApiNode, IEnumerable<ApiNodeComparison> memberComparison)
+        {
+            OldApiNode = oldApiNode;
+            NewApiNode = newApiNode;
+            MemberComparison = memberComparison.ToList();
+        }
+
+        /// <returns>A list of nodes with an <see cref="ApiNodeComparison"/> for each <seealso cref="ApiNode"/> in either the <paramref name="oldApi"/> or <paramref name="newApi"/></returns>
+        public static IReadOnlyCollection<ApiNodeComparison> Compare(IEnumerable<ApiNode> oldApi, IEnumerable<ApiNode> newApi)
+        {
+            var nodeComparisons = new List<ApiNodeComparison>();
+            var newApiByName = new ConcurrentDictionary<string, ApiNode>(newApi.ToDictionary(node => node.Name, node => node));
+            foreach (var oldApiNode in oldApi)
+            {
+                ApiNode newApiNode;
+                var inNewApi = newApiByName.TryRemove(oldApiNode.Signature, out newApiNode);
+                if (inNewApi)
+                {
+                    var memberComparison = Compare(oldApiNode.Members, newApiNode.Members);
+                    nodeComparisons.Add(new ApiNodeComparison(oldApiNode, newApiNode, memberComparison));
+                }
+                else
+                {
+                    nodeComparisons.Add(new ApiNodeComparison(oldApiNode, newApiNode, Compare(oldApiNode.Members, new ApiNode[0])));
+                }
+            }
+
+            nodeComparisons.AddRange(newApiByName.Values
+                .Select(newApiNode => new ApiNodeComparison(null, newApiNode, Compare(new ApiNode[0], newApiNode.Members))));
+
+            return nodeComparisons;
+        }
+
+        private string Describe(Func<ApiNode, string> func)
+        {
+            if (OldApiNode == null) return func(NewApiNode);
+            if (NewApiNode == null) return func(OldApiNode);
+
+            var oldDescription = func(OldApiNode);
+            var newDescription = func(NewApiNode);
+            return oldDescription != newDescription
+                ? $"{oldDescription} -> {newDescription}"
+                : oldDescription;
+        }
+
+        public string Kind => Describe(node => node.Kind.ToString());
+
+        public string Name => Describe(node => node.Name.ToString());
+
+        public string Namespace => Describe(node => node.Namespace.ToString());
+
+        public string Signature => Describe(node => node.Signature.ToString());
+
+        public string SymbolAccessibility => Describe(node => node.SymbolAccessibility.ToString());
+
+        public override string ToString()
+        {
+            return $"{Name}: {SymbolAccessibility}, {Kind}";
+        }
+
+    }
+}
