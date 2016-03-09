1c8d726afaf0e91a4bc65366981b18968535231b
GrahamTheCoder <grahamthecoder@gmail.com>
2016-03-09 10:47:21 +0000

    Add an extension for getting a dictionary from a lookup

---
+++ PublicApiWriter/PublicApiWriter/Extensions/LookupExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyApi.Extensions
+{
+    internal static class LookupExtensions
+    {
+        public static Dictionary<TKey, List<TValue>> ToDictionary<TKey, TValue>(this ILookup<TKey, TValue> lookup)
+        {
+            return lookup.ToDictionary(x => x.Key, x => x.ToList());
+        }
+    }
+}
